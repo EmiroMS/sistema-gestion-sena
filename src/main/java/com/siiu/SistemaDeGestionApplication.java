52c5aa32ec99511ec9d62045dafdc88e5283fffd
EmiroMS <EmiroMS>
2025-11-26 12:01:07 -0500

    Proyecto creado

---
+++ src/main/java/com/siiu/SistemaDeGestionApplication.java
@@ -0,0 +1,13 @@
+package com.siiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siiu/SistemaDeGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
